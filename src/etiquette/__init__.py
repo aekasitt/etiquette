bfd2098e289af7a6f17907981b22ac5f22dbe37e
Sitt Guruvanich <aekazitt@gmail.com>
2025-07-19 14:01:45 +0700

    chore: initial commit

---
+++ src/etiquette/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.9
+# Copyright (C) 2025 All rights reserved.
+# FILENAME:    ~~/src/etiquette/__init__.py
+# VERSION:     0.0.1
+# CREATED:     2025-07-19 13:57
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
+#
+# HISTORY:
+# *************************************************************
+"""
+Semaphored background queue for Asynchronous Server Gateway Interface (ASGI) frameworks
+"""
+
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.9
+# Copyright (C) 2025 All rights reserved.
+# FILENAME:    ~~/src/etiquette/__init__.py
+# VERSION:     0.0.1
+# CREATED:     2025-07-19 13:57
+# AUTHOR:      Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION: https://www.w3docs.com/snippets/python/what-is-init-py-for.html
+#
+# HISTORY:
+# *************************************************************
+"""
+Tests for Etiquette plugin for ASGI frameworks
+"""
